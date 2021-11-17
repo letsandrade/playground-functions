3fbee77598ccc66d93b6f28289d53d4c2df8a70b
leticiaandrade <leticia933@gmail.com>
2021-11-17 16:26:18 -0300

    requisito 6

--- src/challenges.js
+++ src/challenges.js
@@ -24,7 +24,7 @@ console.log(splitSentence("go trybe"))
 
 // Desafio 4 ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array
 function concatName(arrNomes) {
-  let strNome = (arrNomes[arrNomes.length - 1] + "," + arrNomes[0]);
+  let strNome = (arrNomes[arrNomes.length - 1] + ", " + arrNomes[0]);
   return strNome
 };
 arrTest = ["foguete", "não", "tem", "ré"]
@@ -37,10 +37,21 @@ function footballPoints(wins, ties) {
 };
 
 
-// Desafio 6
-function highestCount() {
-  // seu código aqui
+// Desafio 6 ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/max
+function highestCount(arrNumeros) {
+  let numOcorrencias = 0;
+  let maior = (Math.max(...arrNumeros));
+      for (let i = 0; i < arrNumeros.length; i++) {
+        if (arrNumeros[i] == maior) {
+      numOcorrencias = numOcorrencias+1
+    }
+  }; return numOcorrencias
 }
+//arrTest2 = [9, 1, 2, 3, 9, 5, 7]
+//console.log(highestCount(arrTest2))
+
+//function getMaxOfArray(arrNumeros) {
+ // return Math.max.apply(null, arrNumeros);}
 
 // Desafio 7
 function catAndMouse() {
